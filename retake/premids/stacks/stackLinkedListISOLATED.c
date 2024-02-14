d564ad4bf14645c3e9c352c5b5996c5dd902d6c9
Jim5e <jamesterenceclarit@email.com>
2024-02-15 00:47:34 +0800

    Add stackLinkedListISOLATED.c implementation

---
+++ retake/premids/stacks/stackLinkedListISOLATED.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct node{
+    char elem;
+    struct node* link;
+}*stack;
+
+void initStack(stack *A);
+void push(stack *A, char elem);
+char pop(stack *A);
+char peek(stack A);
+bool isEmpty(stack A);
+void makeNull(stack *A);
+void displayStack(stack *A);
+
+int main(){
+    stack MAIN;
+    initStack(&MAIN);
+    push(&MAIN, 'X');
+    push(&MAIN, 'X');
+    push(&MAIN, 'X');
+    push(&MAIN, 'V');
+
+    displayStack(&MAIN);
+    return 0;
+}
+
+void initStack(stack *A){
+    *A = NULL;
+}
+
+void push(stack *A, char elem){
+    stack newNode = (stack) malloc (sizeof(struct node));
+
+    if(newNode != NULL){
+        newNode->elem = elem;
+        newNode->link = *A;
+        *A = newNode;
+    }
+}
+
+char pop(stack *A){
+    char deleted = '\0';
+    if(!isEmpty(*A)){
+        stack temp = *A;
+        (*A) = temp->link;
+        deleted = temp->elem;
+        free(temp);
+    }
+    return deleted;
+}
+
+char peek(stack A){
+    return A->elem;
+}
+
+bool isEmpty(stack A){
+    return (A == NULL)? true : false;
+}
+
+void makeNull(stack *A){
+    *A = NULL;
+}
+
+void displayStack(stack *A){
+    stack temp;
+    initStack(&temp);
+
+    while(!isEmpty(*A)){
+        push(&temp,pop(A));
+    }
+
+    while(!isEmpty(temp)){
+        char val = peek(temp);
+        printf("%c", val);
+        push(A,pop(&temp));
+    }
+}
