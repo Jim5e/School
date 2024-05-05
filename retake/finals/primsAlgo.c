12d20a0ec4a5efaf1f6d5134d273b23ec64c15b9
jim5e <jamesterenceclarit@gmail.com>
2024-05-05 22:24:33 +0800

    Refactor Prims Algorithm implementation and add comments for better readability

--- retake/finals/primsAlgo.c
+++ retake/finals/primsAlgo.c
@@ -1,25 +1,137 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 10
+/************************************************************************
+ * Prim's Algorithm : It grows a spanning tree, one edge at a time.     *
+ * Given:                                                               *
+ *   1)  Graph G = (V, E)                                               *
+ *   2)  V = {0, 1, 2, 3, 4}                                            *
+ *                                                                      *
+ * Steps of Prim�s Algorithm:                                           *
+ *   1) Initialize set U to contain { 0 }                               *
+ *      //In the program, starting vertex will be inputted by the user  *
+ *   2) While (U != V)                                                  *
+ *       a) Find the minimum cost edge (u, v) such that                 *
+ *          u is an element  U and   v is not yet in U                  *
+ *       b)  Add v to U                                                 *
+ ************************************************************************/
+ 
+#include <unistd.h>    // for sleep(), to add in code if display leaves blank screen  
+#include <stdio.h> 
+#include <string.h>   
 
-//DS needed in dijkstras are: SET, ARRAY, 2d ARRAY
-#define GSIZE 4
-#define INF 9999
+                 
+#define MAX  6              // Number of vertices in the graph  
+#define INFINITY  0XFFFF    
+   
+/***** Data Structure Definition *****/  
+typedef int graphType[MAX][MAX];    // A[x][y] = INFINITY  if there is not edge ;  A[x][y] = weight of the edge (x,y)
+typedef int set[MAX];               // Bit-vector implementation of Set
 
-typedef int SET[GSIZE]; // Bit-vector implementation of SET
-typedef int Graph[GSIZE][GSIZE]; // 2D array
-typedef int Vertex; // Chosen vertex
-typedef int distances[GSIZE]; // Distances from the chosen vertex to all
-void printSolution(int dist[][GSIZE]);
-Graph *Prim(Graph A, Vertex V);
+typedef struct {
+int u, v;     // (u,v) represents the edge  
+int weight;   // weight of the edge
+}edgetype;
 
+typedef struct {
+    edgetype edges[MAX];
+    int edgeCount;   //actual number of edges
+int minCost; //Cost of the MST
+}primMST;
 
-int main(){
+/***** Function Prototypes *****/
 
+void populateGraph(graphType G);
+primMST primAlgo(graphType graph, int startVertex);
+void displayPrimMST(primMST tree);
 
-    return 0;
+int main() 
+{ 
+     /*---------------------------------------------------------------------------------
+    * Problem #2 ::  a) Allows user to input from keyboard the starting vertex      *
+    *                  b) Calls populateGraph() to populate the given graph           *
+    *                  c) Calls primAlgo() and displayPrimMST()                       *
+    *                                                                                 *
+    * printf("\n\n\nProblem #2:: ");                                                  *  
+    * printf("\n------------");                                                       *
+    *---------------------------------------------------------------------------------*/
+ printf("\n\n\nProblem #2:: "); 
+   printf("\n------------");
+   graphType G;
+   populateGraph(G);
+
+   primMST P = primAlgo(G, 3);
+   displayPrimMST(P);
+ 
+    return 0; 
+} 
+
+/************************************************************
+ * Function Definitions                                     *
+ ************************************************************/
+void populateGraph(graphType G)
+{
+graphType graph = {  INFINITY, 1, 5, 4, 6, 5, 1, INFINITY, 5, INFINITY, INFINITY, 6, 5, 5, INFINITY, 2, INFINITY, INFINITY, 4,
+                             INFINITY, 2, INFINITY, 1, INFINITY, 6, INFINITY,INFINITY, 1, INFINITY, 3, 5, 6, INFINITY, INFINITY, 3, INFINITY} ;   //5
+
+memcpy(G, graph, sizeof(graph));
 }
 
-Graph *Prim(Graph A, Vertex V){
-    //
+ 
+
+primMST primAlgo(graphType graph, int startVertex)
+{
+ //Write code here
+ primMST P;
+ P.edgeCount = 0;
+ P.minCost = 0;
+
+ //init SET
+ set V = {0};
+ V[startVertex] = 1;
+
+ //find minimum in unvisited nodes
+    int min = INFINITY;
+    int minDx;
+    int u,v;
+
+    for(int count = 0; count < MAX -1 ; count++){
+        min = INFINITY;
+        for(int i = 0; i < MAX; i++){
+            for(int k = 0; k < MAX && V[i] == 1; k++){
+                if(V[k] == 0 && graph[i][k] < min){
+                    //CHECK IF MIN
+                        min = graph[i][k];
+                        minDx = k;
+                        u = i;
+                        v = k;
+                }
+            }
+        }
+        V[minDx] = 1;
+
+            //add to minDx to V and its data to MST
+            if(P.edgeCount != MAX){
+            P.minCost = P.minCost + min;
+            P.edges[P.edgeCount].weight = min; 
+            P.edges[P.edgeCount].u = u;
+            P.edges[P.edgeCount].v = v;
+            P.edgeCount++;
+            }
+    }
+
+    return P;
+}
+
+void displayPrimMST(primMST T)
+{
+int x;
+
+printf("\n%5s%5s\n", "Edge", "Cost");
+ 
+    //Write code here
+    for(int i  = 0; i < T.edgeCount; i++){
+        printf("(%d,%d) ", T.edges[i].u, T.edges[i].v);
+        printf("%-4d\n", T.edges[i].weight);
+    }
+
+    printf("\n\nTotal: %d", T.minCost);
+
 }
