202bc4d763da1027d2c75626df2693d19dd858ea
Your Name <jamesterenceclarit@gmail.com>
2024-10-03 01:27:09 +0800

    fix: please fix winner fighting up :(

--- DSA/Algo and Complexity/tournament_sort.c
+++ DSA/Algo and Complexity/tournament_sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 6
+#define MAX 7
 #define INF 9999999
 
 void print(int *arr, int count);
@@ -8,7 +8,7 @@ void tournamentSort(int *arr);
 
 
 int main(){
-    int arr[] = {4, 3, 6, 1, 10, 5};
+    int arr[] = {4, 3, 6, 1, 10, 5, 7};
     // int arr[] = {10, 2, 7, 6, 5, 9, 12, 1};
 
     printf("BEFORE: ");
@@ -23,7 +23,8 @@ int main(){
 
 void tournamentSort(int *arr){
     //create new array of size 2n-1
-    int tourSize = MAX * 2 - 1;
+    
+    int tourSize = (MAX % 2 == 0? MAX * 2 - 1: MAX * 2);
     int *tour = calloc(tourSize, sizeof(int));
 
     //transfer data from og to tournament array
@@ -32,32 +33,57 @@ void tournamentSort(int *arr){
         tour[y] = arr[x];
     }
 
-    int LLPndx;
-    
-    for(int sortedcount = 0; sortedcount < MAX; sortedcount++){//for every item in the array
-        int LLPndx = (tourSize / 2) - 1;
-        int parentsWithNdxChildren = (LLPndx / 2) - 1;
-            for(; LLPndx > parentsWithNdxChildren; LLPndx--){//traverse and compete via children
-                //select smallest child ndx of that parent
-                int LCndx = LLPndx * 2 + 1;
-                int RCndx = LCndx + 1;
+    //set up the tree: build every node and parent
+    int LLPndx = (tourSize / 2) - 1;
+    int parentsWithNdxChildren = (LLPndx / 2) - 1;
+    for(; LLPndx > parentsWithNdxChildren; LLPndx--){//traverse and compete via children
+        //select smallest child ndx of that parent
+        int LCndx = LLPndx * 2 + 1;
+        int RCndx = LCndx + 1;
 
-                tour[LLPndx] = ((RCndx <= tourSize - 1) && tour[RCndx] < tour[LCndx] ? RCndx : LCndx);
-
-
-            }
-
-            //comparing children with index innards
-            for(;parentsWithNdxChildren >= 0; parentsWithNdxChildren--){
-                int LCndx = parentsWithNdxChildren * 2 + 1;
-                int RCndx = LCndx + 1;
-                tour[parentsWithNdxChildren] = (tour[tour[RCndx]] < tour[tour[LCndx]]? tour[RCndx]: tour[LCndx]); //we can get away with direct comparison since we know L and R exists
-            }
-
-            //at this point the root should be established
-            arr[sortedcount] = tour[tour[parentsWithNdxChildren + 1]];
-            tour[tour[parentsWithNdxChildren + 1]] = INF;
+        tour[LLPndx] = ((RCndx <= tourSize - 1) && tour[RCndx] < tour[LCndx] ? RCndx : LCndx);
     }
+
+    //comparing children with index innards
+    for(;parentsWithNdxChildren >= 0; parentsWithNdxChildren--){
+        int LCndx = parentsWithNdxChildren * 2 + 1;
+        int RCndx = LCndx + 1;
+        tour[parentsWithNdxChildren] = (tour[tour[RCndx]] < tour[tour[LCndx]]? tour[RCndx]: tour[LCndx]); //we can get away with direct comparison since we know L and R exists
+    }
+
+    //at this point the root should be established
+    arr[0] = tour[tour[parentsWithNdxChildren + 1]];
+    tour[tour[parentsWithNdxChildren + 1]] = INF;
+
+
+
+    ///////let ONLY the winner subtree fight going up
+    for(int sortedcount = 1 ; sortedcount < MAX; sortedcount++){
+        int winnerndx = tour[0]; //previous winner
+
+        //directly compare children (children here would be either INF vs INF, or INF vs X where X is a real number)
+        int parentndx = (winnerndx / 2) - 1;
+        int LCndx = (parentndx * 2) + 1;
+        int RCndx = LCndx + 1;
+        tour[parentndx] = ((RCndx <= tourSize - 1) && tour[RCndx] < tour[LCndx] ? RCndx : LCndx);
+
+        //direct compare children (children here would be indexes now, NOT actual value. indexes.)
+        int child = tour[parentndx];
+        for(;parentndx >= 0; parentndx = (child / 2) - 1){
+            int parentndx = (child / 2) - 1;
+            int LCndx = parentndx * 2 + 1;
+            int RCndx = LCndx + 1;
+            tour[parentndx] = (tour[tour[RCndx]] < tour[tour[LCndx]]? tour[RCndx]: tour[LCndx]);
+
+            //
+            child = parentndx;
+        }
+
+        arr[sortedcount] = tour[tour[0]];
+        tour[tour[0]] = INF;
+    }
+        
+
 
 }
 
