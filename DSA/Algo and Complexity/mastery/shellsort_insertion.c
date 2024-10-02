969b979c733cf6376930ce65d6d3b2881c5585cf
Your Name <jamesterenceclarit@gmail.com>
2024-10-03 00:24:25 +0800

    finished :)

--- DSA/Algo and Complexity/mastery/shellsort_insertion.c
+++ DSA/Algo and Complexity/mastery/shellsort_insertion.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX 11
+#define MAX 12
 
 
 void print(int *arr, int count);
@@ -8,14 +8,55 @@ void shell(int *arr);
 
 
 int main(){
-    int arr[] = {62,	83,	18,	53,	7	,17,	95	,86,	47,	69,	25, 28}; //change max to 12
+    int arr[] = {62, 83, 18, 53, 7, 17, 95, 86, 47, 69,	25, 28}; //change max to 12
 
-
+    // insertion(arr);
+    shell(arr);
+    print(arr, MAX);
 
     return 0;
 }
 
-vo
+void shell(int *arr){
+    int count = MAX;
+    int c = 0;
+    
+    for(int gap = count / 2; gap >= 1; gap = gap / 2){
+        printf("gap: %d\n", gap);
+        for(int x = gap; x < count; x += gap){ //equivalent to starting at index 1 of a insertion sort
+            int temp = arr[x];
+            int goingback;
+
+            for(goingback = x - gap; goingback >= 0 && temp < arr[goingback]; goingback = goingback - gap){
+                arr[goingback + gap] = arr[goingback];
+                c++;
+            }
+
+            arr[goingback + gap] = temp;
+        }
+
+    }
+    printf("# of comparison</stdio.h>s: %d\n", c);
+}
+
+void insertion(int *arr){
+    int count = MAX;
+    int c = 0;
+
+    for(int x = 1; x < count;x++){
+        int temp = arr[x];
+        int goingback;
+
+        for(goingback = x - 1; goingback >= 0 && temp < arr[goingback]; goingback--){
+            arr[goingback + 1] = arr[goingback];
+            c++;
+        }
+
+        arr[goingback + 1] = temp;
+    }
+
+    printf("# of comparisons: %d\n", c);
+}
 
 void print(int *arr, int count){
 
